f5b496ec3ddaab8214811ca8cfd823f54aeeadb8
gabrielysantos8 <gabrielysantosp7@gmail.com>
2025-09-02 08:46:35 -0300

    atualizar

--- listar.php
+++ listar.php
@@ -27,7 +27,7 @@
                     echo " 
                     <td>
                         <div class='btn-group' role='group'>
-                            <a href='form_atualiza.php?id=XXX' type='button' class='btn btn-danger'>Atualizar</a>
+                            <a href='form_atualiza.php?id=". $produto['id'] ."' type='button' class='btn btn-danger'>Atualizar</a>
                             <a href='#' type='button' class='btn btn-warning'>Apagar</a>
                         </div>
                     </td>
