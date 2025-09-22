e68d7e9962de7882d4785726e09a2c45655de5cf
gabrielysanto8 <gabrielysantosp7@gmail.com>
2025-09-22 08:45:17 -0300

    front

--- atualizar.php
+++ atualizar.php
@@ -1,41 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
-</head>
-<body>
-    
-</body>
-</html>
-
 <?php
-    require 'conexao.php';
-
-    $id = $_GET['id'];
-    $nome_novo = $_POST['nome_novo'];
-    $preco_novo = $_POST['preco_novo'];
-    $quantidade_novo = $_POST['quantidade_novo'];
-    
-    $sql = "UPDATE produtos SET nome = :nome_novo, preco = :preco_novo, quantidade = :quantidade_novo WHERE id = :id";
-
-    $stmt = $pdo->prepare($sql);
-
-    $stmt->bindParam(':id', $id);
-    $stmt->bindParam(':nome_novo', $nome_novo);
-    $stmt->bindParam(':preco_novo', $preco_novo);
-    $stmt->bindParam(':quantidade_novo', $quantidade_novo);
-
-    if ($stmt->execute()) {
-        echo "Produto atualizado com sucesso! <br>";
-    } else {
-        echo "Erro ao atualizar produto. <br>";
-    }
+include 'pedaco.php';
 ?>
-<a href="listar.php" type="button" class="btn btncor2">Ver lista</a>
+
+<div style="display: grid; place-items: center; margin-top: 200px">
+    <?php
+        require 'conexao.php';
+
+        $id = $_GET['id'];
+        $nome_novo = $_POST['nome_novo'];
+        $preco_novo = $_POST['preco_novo'];
+        $quantidade_novo = $_POST['quantidade_novo'];
+        
+        $sql = "UPDATE produtos SET nome = :nome_novo, preco = :preco_novo, quantidade = :quantidade_novo WHERE id = :id";
+
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':nome_novo', $nome_novo);
+        $stmt->bindParam(':preco_novo', $preco_novo);
+        $stmt->bindParam(':quantidade_novo', $quantidade_novo);
+
+        if ($stmt->execute()) {
+            echo "Produto atualizado com sucesso! <br>";
+        } else {
+            echo "Erro ao atualizar produto. <br>";
+        }
+    ?>
+    <a href="listar.php" type="button" class="btn btncor2">Ver lista</a>
+</div>
